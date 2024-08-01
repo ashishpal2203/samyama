4c251d8c9188446da391a838f206d6795f94bdae
Mohd Faisal <mohdfaisal10769@gmail.com>
2024-08-02 00:35:41 +0530

    change

--- travel-visa-imigration.php
+++ travel-visa-imigration.php
@@ -258,7 +258,7 @@
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target="#flush-collapseonee" aria-expanded="false" aria-controls="flush-collapseonee">
                             Student Visa
-                        </button>
+                        </button>  
                     </h2>
                     <div id="flush-collapseonee" class="accordion-collapse collapse" aria-labelledby="flush-headingonee"
                         data-bs-parent="#accordionExample">
